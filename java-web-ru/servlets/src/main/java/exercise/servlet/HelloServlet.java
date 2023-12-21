356cc9edd9d7c0adb6cf69bfb74aacf08c3b09e1
@hexlet/cli <support@hexlet.io>
2023-12-21 17:59:00 +0300

    submit java-web-ru/servlets

---
+++ java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package exercise.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    // BEGIN
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        var msg = name.equals(null) ? "Hello, Guest!" : String.format("Hello, %s! ", name);
+        req.setAttribute("message", msg);
+
+        req.getRequestDispatcher("/WEB-INF/hello.jsp").forward(req, resp);
+    }
+    // END
+}
