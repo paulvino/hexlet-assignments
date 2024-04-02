37ea84de593f88bb5daca1c32b9f454a0ae7b9f0
@hexlet/cli <support@hexlet.io>
2024-04-02 17:21:02 +0300

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -1,6 +1,5 @@
 package exercise.controller;
 
-import exercise.model.Comment;
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,58 +17,66 @@ import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
 @RestController
-@RequestMapping("/posts")
+@RequestMapping(path = "/posts")
 public class PostsController {
     @Autowired
     private PostRepository postRepository;
 
-    @Autowired CommentRepository commentRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-    @GetMapping("")
+    @GetMapping(path = "")
     public List<PostDTO> index() {
         var posts = postRepository.findAll();
         var result = posts.stream()
                 .map(this::toPostDTO)
                 .toList();
-
         return result;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}")
     public PostDTO show(@PathVariable long id) {
         var post = postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
 
-        var dto = new PostDTO();
-        dto.setId(post.getId());
-        dto.setBody(post.getBody());
-        dto.setTitle(post.getTitle());
-
-        var comments = commentRepository.findByPostId(post.getId()).stream()
-                .map(this::toCommentDTO)
-                .toList();
-        dto.setComments(comments);
-
-        return dto;
+        return toPostDTO(post);
     }
 
     private PostDTO toPostDTO(Post post) {
         var dto = new PostDTO();
-
         dto.setId(post.getId());
         dto.setTitle(post.getTitle());
         dto.setBody(post.getBody());
-
+        var comments = commentRepository.findByPostId(post.getId());
+        var commentsDTO = comments.stream()
+                .map((comment) -> {
+                    var commentDTO = new CommentDTO();
+                    commentDTO.setBody(comment.getBody());
+                    commentDTO.setId(comment.getId());
+                    return commentDTO;
+                })
+                .toList();
+        dto.setComments(commentsDTO);
         return dto;
     }
 
-    private CommentDTO toCommentDTO(Comment comment) {
-        var dto = new CommentDTO();
-
-        dto.setId(comment.getId());
-        dto.setBody(comment.getBody());
-
-        return dto;
-    }
+//    private PostDTO toPostDTO(Post post) {
+//        var dto = new PostDTO();
+//
+//        dto.setId(post.getId());
+//        dto.setTitle(post.getTitle());
+//        dto.setBody(post.getBody());
+//
+//        return dto;
+//    }
+//
+//    private CommentDTO toCommentDTO(Comment comment) {
+//        var dto = new CommentDTO();
+//
+//        dto.setId(comment.getId());
+//        dto.setBody(comment.getBody());
+//
+//        return dto;
+//    }
 }
 // END
